404a4272b4468631bc7244d2f7be46619a53d68f
Baha <zaghdoudibaha@gmailcom>
2020-09-24 17:23:08 +0100

    Building Shopping Component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/sharing/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>() ;
+  @ViewChild('InputName') InputName : ElementRef;
+  @ViewChild('InputAmount') InputAmount : ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  receiveIngredient(){
+    const ingName = this.InputName.nativeElement.value;
+    const ingAmount = this.InputAmount.nativeElement.value;
+    this.ingredientAdded.emit(new Ingredient(ingName,ingAmount));
+    
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,5 +16,8 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onReceiveIngredient(event : Ingredient){
+    this.ingredients.push(event);
+  }
 
 }
